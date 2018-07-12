ccec1575e8541276098b12839f94fd453f7c5783
Mark Douglas Mcdonald <mcdomx@me.com>
2018-07-12 21:35:44 +0000

    init

---
+++ application.py
@@ -0,0 +1,15 @@
+import os
+
+from flask import Flask
+from flask_socketio import SocketIO, emit
+
+app = Flask(__name__)
+app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+socketio = SocketIO(app)
+
+# list of all channels
+channel_list = ['general']
+
+@app.route("/")
+def index():
+    return "Project 2: TODO"
