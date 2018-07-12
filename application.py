198adf50e4be30138c428d636ee04360c6475440
Mark McDonald <mcdomx@me.com>
2018-07-12 17:53:36 -0400

    setup

--- application.py
+++ application.py
@@ -1,11 +1,25 @@
 import os
 
-from flask import Flask
+from flask import Flask, session
+from flask_session import Session
 from flask_socketio import SocketIO, emit
 
 app = Flask(__name__)
+
+# Check for environment variables
+# if not os.getenv("DATABASE_URL"):
+#     raise RuntimeError("-- Environment variable DATABASE_URL is not set")
+
+if not os.getenv("FLASK_APP"):
+    raise RuntimeError("-- Environment variable FLASK_APP is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
 app.config["SECRET_KEY"] = os.getenv("SECRET_KEY")
+
 socketio = SocketIO(app)
+Session(app)
 
 # list of all channels
 channel_list = ['general']
